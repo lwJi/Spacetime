a7cb2aea0af0c53d50472e7b7de7f2c96df2bba0
Liwei Ji <jiliwei.phys@gmail.com>
2025-02-04 14:00:31 -0500

    Z4cowCarpet: clean RHS

--- Z4cowCarpet/src/rhs.cxx
+++ Z4cowCarpet/src/rhs.cxx
@@ -2,18 +2,6 @@
 #include <cctk_Arguments.h>
 #include <cctk_Parameters.h>
 
-#ifdef __CUDACC__
-// Disable CCTK_DEBUG since the debug information takes too much
-// parameter space to launch the kernels
-#ifdef CCTK_DEBUG
-#undef CCTK_DEBUG
-#endif
-#endif
-
-#ifdef __CUDACC__
-#include <nvtx3/nvToolsExt.h>
-#endif
-
 #include <array>
 #include <cmath>
 #include <sys/time.h>
@@ -91,91 +79,8 @@ extern "C" void Z4cowCarpet_RHS(CCTK_ARGUMENTS) {
   const CCTK_REAL ceta = eta;
 
   // Loop
-#ifdef __CUDACC__
-  const nvtxRangeId_t range = nvtxRangeStartA("Z4cowCarpet_RHS::rhs");
-#endif
 
 #include "../wolfram/Z4cowCarpet_set_rhs.hxx"
-
-#ifdef __CUDACC__
-  nvtxRangeEnd(range);
-#endif
-
-#if 0
-  // Upwind and dissipation terms
-
-  const auto apply_diss = [&](const GF3D2<const CCTK_REAL> &gf_,
-                              const GF3D2<CCTK_REAL> &gf_rhs_) {
-    switch (diss_order) {
-    case 3: {
-      grid.loop_int_device<0, 0, 0, vsize>(
-          grid.nghostzones,
-          [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-            const vbool mask = mask_for_loop_tail<vbool>(p.i, p.imax);
-            const vreal rhs_old = gf_rhs_(mask, p.I);
-            const vreal rhs_new =
-                rhs_old + epsdiss * Derivs::calc_diss<2>(gf_, mask, p.I, dx);
-            gf_rhs_.store(mask, p.I, rhs_new);
-          });
-      break;
-    }
-    case 5: {
-      grid.loop_int_device<0, 0, 0, vsize>(
-          grid.nghostzones,
-          [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-            const vbool mask = mask_for_loop_tail<vbool>(p.i, p.imax);
-            const vreal rhs_old = gf_rhs_(mask, p.I);
-            const vreal rhs_new =
-                rhs_old + epsdiss * Derivs::calc_diss<4>(gf_, mask, p.I, dx);
-            gf_rhs_.store(mask, p.I, rhs_new);
-          });
-      break;
-    }
-    case 7: {
-      grid.loop_int_device<0, 0, 0, vsize>(
-          grid.nghostzones,
-          [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-            const vbool mask = mask_for_loop_tail<vbool>(p.i, p.imax);
-            const vreal rhs_old = gf_rhs_(mask, p.I);
-            const vreal rhs_new =
-                rhs_old + epsdiss * Derivs::calc_diss<6>(gf_, mask, p.I, dx);
-            gf_rhs_.store(mask, p.I, rhs_new);
-          });
-      break;
-    }
-    case 9: {
-      grid.loop_int_device<0, 0, 0, vsize>(
-          grid.nghostzones,
-          [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-            const vbool mask = mask_for_loop_tail<vbool>(p.i, p.imax);
-            const vreal rhs_old = gf_rhs_(mask, p.I);
-            const vreal rhs_new =
-                rhs_old + epsdiss * Derivs::calc_diss<8>(gf_, mask, p.I, dx);
-            gf_rhs_.store(mask, p.I, rhs_new);
-          });
-      break;
-    }
-    default:
-      assert(0);
-    }
-  };
-
-  apply_diss(gf_W, gf_dtW);
-  for (int a = 0; a < 3; ++a)
-    for (int b = a; b < 3; ++b)
-      apply_diss(gf_gamt(a, b), gf_dtgamt(a, b));
-  apply_diss(gf_exKh, gf_dtexKh);
-  for (int a = 0; a < 3; ++a)
-    for (int b = a; b < 3; ++b)
-      apply_diss(gf_exAt(a, b), gf_dtexAt(a, b));
-  for (int a = 0; a < 3; ++a)
-    apply_diss(gf_trGt(a), gf_dttrGt(a));
-  if (!set_Theta_zero)
-    apply_diss(gf_Theta, gf_dtTheta);
-  apply_diss(gf_alpha, gf_dtalpha);
-  for (int a = 0; a < 3; ++a)
-    apply_diss(gf_beta(a), gf_dtbeta(a));
-#endif
 
   const double finish_time = gettime();
 
